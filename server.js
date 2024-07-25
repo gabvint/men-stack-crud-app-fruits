586b21a14af167f4d0b25b3da7605b51ef7d62cf
gabtongol <geybtongol@gmail.com>
2024-07-25 14:26:08 -0400

    added route

---
+++ server.js
@@ -0,0 +1,12 @@
+// import modules 
+const express = require('express');
+
+const app = express();
+
+app.get('/', async (req, res) => {
+    
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+});
